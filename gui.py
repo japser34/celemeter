0d4270fa0be6be3457ed297dde80217a04636c90
japser34 <100087296+japser34@users.noreply.github.com>
2024-12-22 13:38:48 +0100

    THROBECKE IS VEEL BETER DAN CELERACING

--- gui.py
+++ gui.py
@@ -29,14 +29,16 @@ class celemeter:
 
         self.info_button = tk.Button(self.top_frame, text="I", command=self.info)
         self.open_button = tk.Button(self.top_frame, text="Open bestand", command=self.open)
+        self.plot_button = tk.Button(self.top_frame, text="Plot", command=self.plot)
 
         self.file_frames = []
 
-        self.info_button.pack()
-        self.open_button.pack()
+        self.info_button.grid(row=0, column=0)
+        self.open_button.grid(row=0, column=1)
+        self.plot_button.grid(row=0, column=2)
 
-        self.top_frame.pack()
-        self.file_frame.pack()
+        self.top_frame.grid(row=0, column=0)
+        self.file_frame.grid(row=1, column=0)
 
         self.root.mainloop()
 
@@ -51,23 +53,35 @@ class celemeter:
         info_button = tk.Button(file_frame, text="I", command=self.info)
         file_name_label = tk.Label(file_frame, text=file_path)
         column_entry = tk.Entry(file_frame)
-        remove_button = tk.Button(file_frame, text="X", command=self.remove)
+        remove_button = tk.Button(file_frame, text="X", command=lambda: self.remove(file_frame))
         
-        info_button.pack()
-        file_name_label.pack()
-        column_entry.pack()
-        remove_button.pack()
+        info_button.grid(row=0, column=0)
+        file_name_label.grid(row=0, column=1)
+        column_entry.grid(row=0, column=2)
+        remove_button.grid(row=0, column=3)
 
-        self.file_frames.append(file_frame)
+        self.file_frames.append([file_frame, info_button, file_name_label, column_entry, remove_button])
         file_frame.pack()
 
 
-    def remove(self):
-        pass
-    
+    def remove(self, file_frame):
+        self.debug(f"file_frames: {self.file_frames}")
+        self.debug(f"removing {file_frame}")
+        for i in range(len(self.file_frames)):
+            self.debug(f"checking {i}")
+            if self.file_frames[i][0] == file_frame:
+                self.debug(f"removing {i}")
+                self.file_frames.pop(i)
+                file_frame.destroy()
+                break
     def info(self):
         pass
 
+    def plot(self):
+        pass
+    
+    def debug(self, text):
+        print(f"[DEBUG]: {text}")
 
 if __name__ == "__main__":
     root = tk.Tk()
