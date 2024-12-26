f7efc95e1b7a8d4e2d13ae6f914d0be42b9d0e77
jasper <100087296+japser34@users.noreply.github.com>
2024-12-26 10:32:56 +0100

    Update gui.py
    Thorbecke is beter

--- gui.py
+++ gui.py
@@ -270,7 +270,8 @@ class celemeter:
 
     def get_column(self, file_frame):
         self.debug.debug(f'getting column for "{file_frame}"')
-        self.debug.debug(f'returning "{file_frame[2].cget("text")}"')
+        data = file_frame[3].get()
+        
         return file_frame[2].cget("text")
 
     def plot(self): # the data should look like this: [(file_name, x_col, y_col), ...]
